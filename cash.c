e63f731700033e7128bce328061b2e47ef0bdad5
josh-olan <josh-olan@users.noreply.github.com>
2020-08-10 20:29:36 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -2,44 +2,47 @@
 #include <cs50.h>
 #include <math.h>
 
-int main (void)
+int main(void)
 {
     float dollars;
     do 
     {
         //prompt the user for input
-         dollars = get_float ("Change owed: \n");
+        dollars = get_float("Change owed: \n");
     }
     //checks for negative inputs
     while (dollars < 0);
 
-   printf ("Change owed: %.2f\n", dollars);
+    printf("Change owed: %.2f\n", dollars);
 
-   int cents = round(dollars * 100);
+    int cents = round(dollars * 100);
    
-   //initialization of integer to count number of coins
-   int i = 0;
-   while (cents>=25)
-   {
-       cents = cents - 25;
-       i++;
-   }
-
-   while (cents >= 10)
-   {
-       cents = cents - 10;
-       i++;
-   }
-   while (cents >= 5)
-   {
-       cents = cents - 5;
-       i++;
-   }
-   while ( cents >= 1)
-   {
+    //initialization of integer to count number of coins
+    int i = 0;
+    //a while loop to check if 25 cents can be gotten from the leftover change
+    while (cents >= 25)
+    {
+        cents = cents - 25;
+        i++;
+    }
+    //a while loop to check if 10 cents can be gotten from the leftover change
+    while (cents >= 10)
+    {
+        cents = cents - 10;
+        i++;
+    }
+    //a while loop to check if 5 cents can be gotten from the leftover change
+    while (cents >= 5)
+    {
+        cents = cents - 5;
+        i++;
+    }
+    //a while loop to check if 1 cent can be gotten from the leftover change
+    while (cents >= 1)
+    {
        cents = cents - 1;
        i++;
-   }
+    }
 
-   printf("%i\n", i);
+    printf("%i\n", i);
 }
