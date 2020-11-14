021b8f93de7e44771c6f8441318c804330326d44
Josh <josh.olan@yahoo.com>
2020-11-14 23:51:00 +0000

    Adding all the OLAN files

--- network/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import User, Post, Follower, Like
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Follower)
-admin.site.register(Like)
